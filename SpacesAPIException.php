08e74030a1d759edea8aa4686dcd1e25aeef3a9e
azazello <azazellolvv@gmail.com>
2021-03-25 09:15:57 +0200

    Fix file name

---
+++ SpacesAPIException.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Azazello\SpacesConnect;
+
+
+/*
+  INTERNAL FUNCTION - Throws error for catching.
+*/
+
+class SpacesAPIException extends \Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function GetError()
+    {
+        $error = @json_decode($this->getMessage(), true);
+        return $error["error"];
+    }
+}
--- SpacesConnect.php
+++ SpacesConnect.php
@@ -509,21 +509,3 @@ class SpacesConnect
     }
 
 }
-
-/*
-  INTERNAL FUNCTION - Throws error for catching.
-*/
-
-class SpacesAPIException extends \Exception
-{
-    public function __construct($message, $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
-    public function GetError()
-    {
-        $error = @json_decode($this->getMessage(), true);
-        return $error["error"];
-    }
-}
